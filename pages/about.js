182f2c15ed310a3f779402dab2fc65c0f4eb0a72
AbelMejiaCode <49965934+GITABELMejia@users.noreply.github.com>
2023-02-16 13:44:38 -0500

    Add files via upload

--- pages/about.js
+++ pages/about.js
@@ -4,10 +4,46 @@ import Link from "next/link"
 const About = () => (
 
     <Layout>
-         <header className=" about">          
+        <header className="about">
         </header>
         <section>
-            <h1 className="text-center">About</h1>
+            <div className="container mt-5">
+                <div className="row text-center">
+                    <div className="col mb-5">
+                        <h2>LA EMPRESA</h2>
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi sint error nisi pariatur dignissimos? Odio eum quae illo enim dicta nobis necessitatibus obcaecati repudiandae perferendis. Doloribus accusamus doloremque quis aliquid nam sequi molestias autem rem fugit totam sint non sit esse sed nulla ipsum earum natus, nemo repudiandae, modi quae.</p>
+                    </div>
+
+                </div>
+                <div className="row text-center">
+                    <h2 className="mb-4">SOCIOS FUNDADORES</h2>
+                    <div className="col-md-6 mb-5">
+                        <img src="/socio_fundador2.jpg" alt="" className="rounded-circle" />
+                        <h4>Primer Fundador</h4>
+                    </div>
+                    <div className="col-md-6 mb-5">
+                        <img src="/socio_fundador1.jpg" alt="" className="rounded-circle" />
+                        <h4>Segundo fundador</h4>
+                    </div>
+
+                </div>
+                <div className="row text-center">
+                    <h3>EL EQUIPO</h3>
+                    <div className="col-md-4">
+                        <img src="/equipo1.jpg" alt="" className="rounded"/>
+                        <h4>Primera Foto</h4>
+                    </div>
+                    <div className="col-md-4">
+                        <img src="/equipo2.jpg" alt="" className="rounded"/>
+                        <h4>Primera Foto</h4>
+                    </div>
+                    <div className="col-md-4">
+                        <img src="/equipo3.jpg" alt="" className="rounded"/>
+                        <h4>Primera Foto</h4>
+                    </div>
+                </div>
+            </div>
         </section>
     </Layout>
 
