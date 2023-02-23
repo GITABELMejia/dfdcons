0378a8946f0e385e4dcc665967fdafe67cd31b2d
AbelMejiaCode <49965934+GITABELMejia@users.noreply.github.com>
2023-02-23 16:45:03 -0500

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,17 @@
+import Layout from "../components/Layout";
+import { skills, experiences, projects } from "../profile";
+import Link from "next/link"
+const Contact = () => (
+
+    <Layout>
+        <header className=" about">
+        </header>
+        <section>
+            <h1 className="text-center">Contacto</h1>
+            <h2>Actualizado desde GITHUB</h2>
+        </section>
+    </Layout>
+
+)
+
+export default Contact
