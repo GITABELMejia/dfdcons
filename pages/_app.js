ee6d8a8be8c567040027c0e7baf10fb571e66270
AbelMejiaCode <49965934+GITABELMejia@users.noreply.github.com>
2023-02-27 17:24:50 -0500

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
 import 'bootswatch/dist/cosmo/bootstrap.min.css'
 import '../global.css'
 import Head from 'next/head'
+import "@fortawesome/fontawesome-svg-core/styles.css"; 
+
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 
 import { useEffect } from 'react'
 
--- pages/contact.js
+++ pages/contact.js
@@ -1,14 +1,44 @@
 import Layout from "../components/Layout";
 import { skills, experiences, projects } from "../profile";
 import Link from "next/link"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faSearch,
+    faAmbulance,
+    faAnchor,
+} from "@fortawesome/free-solid-svg-icons";
+
 const Contact = () => (
 
     <Layout>
         <header className=" about">
         </header>
         <section>
-            <h1 className="text-center">Contacto</h1>
-            <h2>Actualizado desde GITHUB</h2>
+            <div className="container pt-4">
+                <div className="row text-center ">
+                    <div className="col-md-6">
+                        <h4>DFDCONSTRUCCION</h4>
+                        <address>
+                            "Direccion de la oficina de la Empresa"
+                        </address>
+                        <FontAwesomeIcon
+                            icon={faAmbulance}
+                            style={{ fontSize: 50, color: "orange" }}
+                        />
+                    </div>
+                    <div className="col-md-6">
+                        <h4>FORMULARIO DE CONTACTO</h4>
+                        <div className="row">
+                            <div className="col-md-6">
+                                <p>Primer formulario</p>
+                            </div>
+                            <div className="col-md-6">
+                                Mensaje
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     </Layout>
 
